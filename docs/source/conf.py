f849a345d03c897635c6344ffa6976e0f4c65ec0
Kevin Kramer <kevinpkramer@gmail.com>
2020-08-18 15:53:36 +0200

    fix module name error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../..'))
+sys.path.insert(0, os.path.abspath('./playing_cards'))
+sys.path.insert(0, os.path.abspath('../playing_cards'))
+sys.path.insert(0, os.path.abspath('../../playing_cards'))
+sys.path.insert(0, os.path.abspath('../../../playing_cards'))
 
 
 # -- Project information -----------------------------------------------------
